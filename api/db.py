506830bc56dc32c85efcbbbda0e99ab7ef05a9e2
oyololatoni <oyololatoni@gmail.com>
2026-02-10 23:55:45 +0100

    fix webhook auth (2026-02-10T23:55:45+01:00)

--- api/db.py
+++ api/db.py
@@ -1,7 +1,5 @@
 import psycopg
-import os
-
-DATABASE_URL = os.environ["DATABASE_URL"]
+from config import DATABASE_URL
 
 def get_conn():
     return psycopg.connect(DATABASE_URL, connect_timeout=5)
