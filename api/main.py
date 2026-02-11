233059f9253aada7395dc34b76a62d1060f0f54f
oyololatoni <oyololatoni@gmail.com>
2026-02-11 12:29:23 +0100

    fix webhook auth (2026-02-11T12:29:23+01:00)

--- api/main.py
+++ api/main.py
@@ -8,6 +8,7 @@ from api.metrics import router as metrics_router
 from api.webhooks import router as webhooks_router
 from api.analytics import router as analytics_router
 from api.alerts import router as alerts_router
+from api.screener import router as screener_router
 from config import TRACKED_TOKENS, INGESTION_ENABLED
 
 if not TRACKED_TOKENS:
@@ -47,3 +48,4 @@ app.include_router(metrics_router)
 app.include_router(webhooks_router)
 app.include_router(analytics_router)
 app.include_router(alerts_router)
+app.include_router(screener_router)
